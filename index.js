aada909e0b6fbcf7fa27aa65c680a2f9ef331d5a
Tyler Alder <tyler.b.alder@gmail.com>
2018-06-10 23:01:15 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+var getFirstSelector = function(selector) {
+  return document.querySelector(selector);
+};
+
+var nestedTarget = function() {
+  return document.querySelector('#nested .target');
+};
+
+var increaseRankBy = function(n) {
+  let list = document.querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < list.length; i++) {
+  list[i].innerHTML = parseInt(list[i].innerHTML) + n;
+      }
+};
+
+var deepestChild = function() {
+  let theChild = document.querySelectorAll('#grand-node div');
+  return theChild[(theChild.length) - 1];
+}
